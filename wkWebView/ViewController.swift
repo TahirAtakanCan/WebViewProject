bb58a4d78f95059088a78e2d16f1f3dad439edc0
TahirAtakanCan <atkan4202@gmail.com>
2023-12-28 15:59:45 +0300

    feat: add js code

--- wkWebView/ViewController.swift
+++ wkWebView/ViewController.swift
@@ -8,11 +8,10 @@
 import UIKit
 import WebKit
 
-
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,13 +19,62 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.navigationDelegate = self
         view = webView
         
-        
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://2.elveri.com/")!
         webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true 
-        
+        webView.allowsBackForwardNavigationGestures = true
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Bir gecikme ekleyerek JavaScript kodunu çalıştır
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+            self.webView.evaluateJavaScript(self.addButtonScript) { (result, error) in
+                if let error = error {
+                    print("JavaScript Error: \(error)")
+                }
+            }
+        }
+    }
+    
+    let addButtonScript = """
+        var button = document.createElement('button');
+        button.innerHTML = 'Click me';
+        button.style.padding = '10px';
+        button.style.backgroundColor = 'blue';
+        button.style.color = 'white';
+        var abcElement = document.querySelector('.header-selectors-wrapper');
 
-
+        if (abcElement) {
+            abcElement.appendChild(button);
+            button.addEventListener('click', function() {
+                console.log("atakan");
+                // Swift kodunu çağır
+                window.webkit.messageHandlers.buttonClicked.postMessage(null);
+            });
+        }
+    """
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "JS", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "addButtonScript", style: .default, handler: clickedButton))
+        present(ac, animated: true)
+    }
+    
+    func clickedButton(action: UIAlertAction) {
+        // Bu metodun içine tıklanan butonun özel işlevselliği eklenir (örneğin, alert gösterme)
+        let alertController = UIAlertController(title: "Button Clicked", message: "Button was clicked!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // JavaScript tarafından postMessage ile gönderilen mesajı işle
+        if message.name == "buttonClicked" {
+            // JavaScript tarafından gönderilen mesaj, burada işlenir
+            openTapped()
+        }
+    }
+}
